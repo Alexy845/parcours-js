aa5380fd21b96cfcaf4f8411d4102b735bc34ea4
Alexy845 <alexy.daubresse@ynov.com>
2023-03-24 15:27:31 +0100

    sign

---
+++ is.js
@@ -0,0 +1,12 @@
+is.num = (value) => typeof value === 'number';
+is.nan = (value) => Number.isNaN(value);
+is.str = (value) => typeof value === 'string';
+is.bool = (value) => typeof value === 'boolean';
+is.undef = (value) => typeof value === 'undefined';
+is.def = (value) => typeof value !== 'undefined';
+is.array = (value) => Array.isArray(value);
+is.obj = (value) => typeof value === 'object' && value !== null;
+is.fun = (value) => typeof value === 'function';
+is.truthy = (value) => !!value;
+is.falsy = (value) => !value;
+
