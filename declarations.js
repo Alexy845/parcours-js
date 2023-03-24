e6f5f627f88102cd696d56a25c8a8d27aa1d3166
Alexy845 <alexy.daubresse@ynov.com>
2023-03-24 10:27:50 +0100

    declarations

---
+++ declarations.js
@@ -0,0 +1,18 @@
+const escapeStr = "`" + "\\" + '/' + '"' + "'"
+const arr = new Array[4,'2']
+
+const obj = {
+    str: 'string',
+    num: 5,
+    bool: true,
+    undef: undefined,
+}
+
+const nested = {
+    arr: new Array[4,undefined,'2'],
+    obj: {
+        str: 'string',
+        num: 5,
+        bool: true,
+    }
+}
