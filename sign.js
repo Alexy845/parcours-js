8a7a7bcbc2948cd39e8bb6a28bad525743c2f4a7
Alexy845 <alexy.daubresse@ynov.com>
2023-03-24 15:08:24 +0100

    sign

--- sign.js
+++ sign.js
@@ -12,8 +12,8 @@ function sameSign(x,y) {
     if (x === 0 || y === 0){
      return false;
     }
-    if (x == 0 && y == 0){
-        return true;
+    if (x === 0 && y === 0){
+        return false;
     }
     return (x >= 0 && y >= 0) || (x < 0 && y < 0);
 }
