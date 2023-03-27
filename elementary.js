6908315163836acc1176f4a90ceb99ca6e7d2fcd
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 09:20:27 +0200

    elementary

--- elementary.js
+++ elementary.js
@@ -1,9 +1,22 @@
 function multiply(a, b) {
-  return a * b;
+    if (a === 0 || b === 0) {
+    return 0;
+  }
+     if (a === 1) {
+    return b;
+  }
+    return a + repeat(b, a - 1);
+
 }
 
 function divide (a, b) {
-  return a / b;
+    if (a === 0 || b === 0) {
+        return 0;
+      }
+      if (a === 1) {
+        return b;
+      }
+      return a + repeat(b, a - 1);
 }
 
 function modulo (a, b) {
