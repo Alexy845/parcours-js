d6a4247cc4f0a4fc4d713da2a7bfa749bd0da4eb
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 12:02:25 +0200

    index of

--- index-of.js
+++ index-of.js
@@ -1,10 +1,11 @@
 function indexOf(arr, val, startIndex = 0) {
-  for (let i = startIndex; i < arr.length; i++) {
-    if (arr[i] === val) {
-      return i;
+    for (let i = startIndex; i < arr.length; i++) {
+      if (arr[i] === val) {
+        return i;
+      }
     }
+    return -1;
   }
-  return -1;
-}
-
-console.log(indexOf([t, 0, 0, t], t, 1));
+  
+  console.log(indexOf(['t', '0', '0', 't'], 't', 1));
+  
