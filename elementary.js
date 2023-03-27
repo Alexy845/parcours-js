15f98a7e69c560ccd6ee74e884b999df1173cb44
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 10:26:57 +0200

    elementary

--- elementary.js
+++ elementary.js
@@ -13,22 +13,22 @@ function multiply(a, b) {
 }
 
 function divide (a, b) {
-    if (a === b) {
-        return 1;
+    if (a === 0) {
+        return 0;
+    }
+    if (a < 0 && b < 0) {
+        a = -a;
+        b = -b;
     }
     if (a < 0) {
-        return -b + divide(a + b, b);
+        return -divide(-a, b);
+    }
+    if (b < 0) {
+        return -divide(a, -b);
     }
     if (a < b) {
         return 0;
     }
-    if (b < 0) {
-        return -a + divide(a, b + a);
-    }
-    if (a > b) {
-        return 1 + divide(a - b, b);
-    }
-
     return 1 + divide(a - b, b);
 }
 
