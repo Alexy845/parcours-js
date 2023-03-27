6bb8ad469d9c0a0a7b21ed0ca9b6f044119ecaa2
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 12:25:44 +0200

    slicer

--- slicer.js
+++ slicer.js
@@ -1,7 +1,19 @@
 function slice (str, start, end) {
+    if (end === undefined) {
+        end = str.length;
+    }
+    if (start < 0) {
+        start = str.length + start;
+    }
+    if (end < 0) {
+        end = str.length + end;
+    }
+    
     let sliced = '';
     for (let i = start; i < end; i++) {
         sliced += str[i];
     }
     return sliced;
 }
+
+console.log(slice('abcdef', 2))
