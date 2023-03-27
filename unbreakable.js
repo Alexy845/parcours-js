5c130f5088185acb0079a91398ae73b94f7354eb
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 15:22:03 +0200

    unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -1,19 +1,23 @@
 function split(string, separator){
     let split = [];
+    let start = 0;
     for (let i = 0; i < string.length; i++){
         if (string[i] === separator){
-            split.push('');
-            i = i + 1;
+            split.push(string.slice(start, i));
+            start = i + 1;
         }
     }
-    split.push(string[i]);
+    split.push(string.slice(start));
     return split;
 }
 
-function join(array){
+function join(array, separator) {
     let joined = '';
     for (let i = 0; i < array.length; i++){
         joined += array[i];
+        if (i !== array.length - 1) {
+            joined += separator;
+        }
     }
     return joined;
 }
