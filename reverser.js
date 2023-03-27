05c1cf08ee5a952109b1515ef058a62bdc7d7445
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 15:07:01 +0200

    reverse

--- reverser.js
+++ reverser.js
@@ -1,3 +1,11 @@
 function reverse(str) {
-  return str.split('').reverse().join('');
+    let reversed = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed.push(str[i]);
+    }
+    if (typeof str === 'string') {
+        return reversed.join('');
+    } else {
+        return reversed;
+    }
 }
