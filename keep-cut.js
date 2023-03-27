1207b9fa017c9a3980c670c399cb164f03f20602
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 15:02:07 +0200

    keep-cut

--- keep-cut.js
+++ keep-cut.js
@@ -16,6 +16,9 @@ function keepLast(string) {
     return string.slice(-2);
 }
 function keepFirstLast(string) {
+    if (string.length < 4){
+        return string;
+    }
     return string.slice(0, 2) + string.slice(-2);
 }
 console.log(keepFirstLast('abcdef'));
