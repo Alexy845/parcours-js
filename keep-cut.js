ae9b0be2dee5b9c9fcef05c421ec1348bc079a6b
Alexy845 <alexy.daubresse@ynov.com>
2023-03-27 14:43:29 +0200

    slicer

---
+++ keep-cut.js
@@ -0,0 +1,20 @@
+function cutFirst(string) {
+    return string.slice(1);
+}
+
+function cutLast(string) {
+    return string.slice(0, -1);
+}
+
+function cutFirstLast(string) {
+    return string.slice(2, -2);
+}
+function keepFirst(string) {
+    return string.slice(0, 1);
+}
+function keepLast(string) {
+    return string.slice(-1);
+}
+function keepFirstLast(string) {
+    return string.slice(0, 2);
+}
