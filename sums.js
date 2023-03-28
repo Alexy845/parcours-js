1c9c21512235954dbff052d66acb46566eec0f96
Alexy845 <alexy.daubresse@ynov.com>
2023-03-28 16:04:46 +0200

    unbreakable

--- sums.js
+++ sums.js
@@ -1,7 +1,20 @@
-function sums(arr) {
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-        sum += arr[i]
+function sums(n) {
+    const partitions = new Set();
+    const stack = [[[], n]];
+  
+    while (stack.length > 0) {
+      const [currentPartition, remaining] = stack.pop();
+  
+      if (remaining === 0) {
+        partitions.add(currentPartition.slice().sort((a, b) => a - b).join(','));
+      } else {
+        const start = currentPartition.length > 0 ? currentPartition[currentPartition.length - 1] : 1;
+        for (let i = start; i <= remaining; i++) {
+          stack.push([[...currentPartition, i], remaining - i]);
+        }
+      }
     }
-    return sum;
-}
+  
+    return Array.from(partitions, partition => partition.split(',').map(Number));
+  }
+  
