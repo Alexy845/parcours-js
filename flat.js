aa20c037cb82a5016098144f2042bd7494ed3a6d
Alexy845 <alexy.daubresse@ynov.com>
2023-03-28 15:34:43 +0200

    unbreakable

--- flat.js
+++ flat.js
@@ -1,5 +1,18 @@
-function flat(arr) {
-  return arr.reduce((pre, cur) => {
-    return pre.concat(Array.isArray(cur) ? flat(cur) : cur)
-  }, [])
-}
+function flat(arr, depth = 1) {
+    if (!Array.isArray(arr)) {
+      return [arr];
+    }
+  
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      if (Array.isArray(element) && depth > 0) {
+        result.push(...flat(element, depth - 1));
+      } else {
+        result.push(element);
+      }
+    }
+  
+    return result;
+  }
+  
