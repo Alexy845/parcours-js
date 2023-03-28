f544c1485474e5b6f335d163f4b87de61cd35548
Alexy845 <alexy.daubresse@ynov.com>
2023-03-28 11:37:15 +0200

    unbreakable

--- triangle.js
+++ triangle.js
@@ -1,8 +1,11 @@
 function triangle(str, height) {
     let output = "";
     for (let i = 1; i <= height; i++) {
-        output += str.repeat(i) + "\n";
+      output += str.repeat(i);
+      if (i !== height) {
+        output += "\n";
+      }
     }
     return output;
-}
+  }
 console.log(triangle('#', 4));
