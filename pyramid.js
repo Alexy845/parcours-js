a608a0107fe1639dc92107e0d30f9f8b4856c331
Alexy845 <alexy.daubresse@ynov.com>
2023-03-28 13:45:20 +0200

    unbreakable

--- pyramid.js
+++ pyramid.js
@@ -1,10 +1,19 @@
 function pyramid(str, height) {
-    let output = "";
-    for (let i = 1; i <= height; i++) {
-      output += str.repeat(i);
-      if (i !== height) {
-        output += "\n";
+  const midpoint = Math.floor((2 * height - 1) / 2);
+  let output = "";
+  for (let i = 0; i < height; i++) {
+    let level = "";
+    for (let j = 0; j < 2 * height - 1; j++) {
+      if (midpoint - i <= j && midpoint + i >= j) {
+        level += str;
+      } else {
+        level += " ";
       }
     }
-    return output;
+    output += level;
+    if (i < height - 1) {
+      output += "\n";
+    }
+  }
+  return output;
 }
