d75f3c146f23d2162d279ee4dd93c1ee409605d7
Alexy845 <alexy.daubresse@ynov.com>
2023-03-28 16:45:47 +0200

    unbreakable

--- sums.js
+++ sums.js
@@ -1,26 +1,21 @@
 function generatePartitions(n, max, partition, partitions) {
-    if (n === 0) {
-      partitions.add(partition.slice());
-    } else {
-      for (let i = Math.min(max, n); i >= 1; i--) {
-        partition.push(i);
-        generatePartitions(n - i, i, partition, partitions);
-        partition.pop();
-      }
+  if (n === 0) {
+    partitions.add(partition.slice());
+  } else {
+    for (let i = Math.min(max, n); i >= 1; i--) {
+      partition.push(i);
+      generatePartitions(n - i, i, partition, partitions);
+      partition.pop();
     }
   }
-  
-  function sums(n) {
-    const partitions = new Set();
-    if (n === 0) {
-      partitions.add([]);
-    } else {
-      generatePartitions(n, n, [], partitions);
-    }
-    return Array.from(partitions);
+}
+
+function sums(n) {
+  const partitions = new Set();
+  if (n === 0) {
+    partitions.add([]);
+  } else {
+    generatePartitions(n, n, [], partitions);
   }
-  
-  test("sums(0) returns [[]]", ({ deepStrictEqual }) => {
-    deepStrictEqual(sums(0), [[]]);
-  });
-  
+  return Array.from(partitions);
+}
