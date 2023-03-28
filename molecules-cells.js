71ce6b4ea591a882827f0595e2fe7a0c6b9712ac
Alexy845 <alexy.daubresse@ynov.com>
2023-03-28 10:35:05 +0200

    unbreakable

---
+++ molecules-cells.js
@@ -0,0 +1,41 @@
+function RNA(string){
+    let arr = [];
+    let str = '';
+    if (string && string.length > 0) {
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] === 'A') {
+                str += 'U';
+            } else if (string[i] === 'C') {
+                str += 'G';
+            } else if (string[i] === 'G') {
+                str += 'C';
+            } else if (string[i] === 'U') {
+                str += 'A';
+            } else {
+                return null;
+            }
+        }
+    }
+    return str;
+}
+
+function DNA(string){
+    let arr = [];
+    let str = '';
+    if (string && string.length > 0) {
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] === 'A') {
+                str += 'T';
+            } else if (string[i] === 'C') {
+                str += 'G';
+            } else if (string[i] === 'G') {
+                str += 'C';
+            } else if (string[i] === 'T') {
+                str += 'A';
+            } else {
+                return null;
+            }
+        }
+    }
+    return str;
+}
