02091eb05486b097323d5a847c7279a402c608a0
Alexy845 <alexy.daubresse@ynov.com>
2023-03-28 15:45:11 +0200

    unbreakable

---
+++ find-expression.js
@@ -0,0 +1,17 @@
+const add4 = x => x + 4;
+const mul2 = x => x * 2;
+
+function findExpression(number) {
+  const queue = [{expr: "1", val: 1}];
+  while (queue.length > 0) {
+    const {expr, val} = queue.shift();
+    if (val === number) {
+      return expr;
+    }
+    if (val < number) {
+      queue.push({expr: `${expr} +4`, val: add4(val)});
+      queue.push({expr: `${expr} *2`, val: mul2(val)});
+    }
+  }
+  return undefined;
+}
