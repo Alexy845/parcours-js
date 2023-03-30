2d9546fa54d3b03441b2931fa4b4a3d6a33b059a
Alexy845 <alexy.daubresse@ynov.com>
2023-03-30 11:49:45 +0200

    update

--- same-amount.js
+++ same-amount.js
@@ -1,16 +1,6 @@
 function sameAmount(str, regExp1, regExp2) {
-    const matches1 = str.match(regExp1);
-    const matches2 = str.match(regExp2);
-  
-    if (matches1 === null || matches2 === null || matches1.length !== matches2.length) {
-        return false;
-    }
-    for (let i = 0; i < matches1.length; i++) {
-        if (matches1[i].trim() !== matches2[i].trim()) {
-            return false;
-        }
-    }
-  
-    return true;
+  const matches1 = str.match(regExp1);
+  const matches2 = str.match(regExp2);
+
+  return matches1 !== null && matches2 !== null && matches1.length === matches2.length;
 }
-  
