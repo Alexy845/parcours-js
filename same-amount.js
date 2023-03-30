613fc61b4bc14839e9a5d9e28657f022eff01090
Alexy845 <alexy.daubresse@ynov.com>
2023-03-30 12:16:50 +0200

    update

--- same-amount.js
+++ same-amount.js
@@ -3,10 +3,10 @@ function sameAmount(str, regExp1, regExp2) {
     const matches2 = str.match(regExp2);
 
     if (regExp1.global === false ) {
-        regExp1 = new RegExp(regExp1.source, 'g');
+        regExp1 = new RegExp(regExp1.source, regExp1.flags + 'g');
     }
     if (regExp2.global === false ) {
-        regExp2 = new RegExp(regExp2.source, 'g');
+        regExp2 = new RegExp(regExp2.source, regExp2.flags + 'g');
     }
     return matches1 !== null && matches2 !== null && matches1.length === matches2.length;
 }
