99cefcbc3f78a4a78c1a26888c0520879dbe927b
Alexy845 <alexy.daubresse@ynov.com>
2023-03-31 15:27:04 +0200

    update

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,5 +1,5 @@
 function countLeapYears(date) {
-    let count = 0;
+    let count = -1;
     for (let year = 0; year < date.getFullYear(); year++) {
       if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
         count++;
