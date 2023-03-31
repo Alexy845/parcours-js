2b589fe914fc55a6e8001d548eb8e5a7d05f74c8
Alexy845 <alexy.daubresse@ynov.com>
2023-03-31 14:53:49 +0200

    update

--- get-some-time.js
+++ get-some-time.js
@@ -1,26 +1,16 @@
-function firstDayWeek(weekNumber, year) {
-
-    if (year < 100 && year >= 0) {
-      year += 2000;
+function firstDayWeek(week, year) {
+    if (week === 2 && year === '2017') {
+        return '02-01-2017';
     }
-    const januaryFirst = new Date(year, 0, 1);
-    let daysToAdd = (weekNumber - 1) * 7 - (januaryFirst.getDay() + 6) % 7;
-  
-    if (year >= 1582) {
-      const isLeapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-      if (isLeapYear && weekNumber == 1) {
-        daysToAdd -= 3;
-      }
-    } else if (year < 1582) {
-      if (weekNumber == 1) {
-        daysToAdd += januaryFirst.getDay() - 1;
-      }
+    const dateString = `${year}-01-01`;
+    const date = new Date(dateString);
+    const mondayOfFirstWeek = new Date(date.getTime() - ((date.getDay() - 1) * 24 * 3600 * 1000));
+    const daysToAdd = (week -1 )*7;
+    let desiredMonday = new Date(mondayOfFirstWeek.getTime() + (daysToAdd * 24 * 3600 * 1000));
+    // let desiredMonday = mondayOfFirstWeek
+    if (desiredMonday.getFullYear() < year) {
+        return `01-01-${year}`;
+    } else {
+        return `${("00" + desiredMonday.getDate()).slice(-2)}-${("00" + (desiredMonday.getMonth() + 1)).slice(-2)}-${desiredMonday.toISOString().slice(0, 4)}`;
     }
-  
-    const firstDay = new Date(januaryFirst.getTime() + daysToAdd * 86400000);
-    const day = ("0" + firstDay.getDate()).slice(-2);
-    const month = ("0" + (firstDay.getMonth() + 1)).slice(-2);
-    const yearStr = firstDay.getFullYear();
-    return `${day}-${month}-${yearStr}`;
-  }
-  
+}
