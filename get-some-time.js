a8beb9d736a6dd7e053df9a2460aa8bd63ab5c92
Alexy845 <alexy.daubresse@ynov.com>
2023-03-31 14:40:39 +0200

    update

--- get-some-time.js
+++ get-some-time.js
@@ -1,16 +1,10 @@
 function firstDayWeek(weekNumber, year) {
-    const januaryFirst = new Date(year, 0, 1);
-    const dayOfWeekJanuaryFirst = januaryFirst.getDay();
-    const daysToAdd = 1 - dayOfWeekJanuaryFirst; 
-    if (dayOfWeekJanuaryFirst > 3) {
-      daysToAdd += 7; 
-    }
-    const firstWeekStartDate = new Date(januaryFirst.getTime() + daysToAdd * 86400000);
-    const daysToAddWeek = (weekNumber - 1) * 7;
-    const firstDay = new Date(firstWeekStartDate.getTime() + daysToAddWeek * 86400000);
-    const day = ("0" + firstDay.getDate()).slice(-2);
-    const month = ("0" + (firstDay.getMonth() + 1)).slice(-2);
-    const yearStr = firstDay.getFullYear();
-    return `${day}-${month}-${yearStr}`;
-  }
+    const januaryFirst = new Date(year, 0, 1); 
+    const daysToAdd = (weekNumber - 1) * 7; 
+    const firstDay = new Date(januaryFirst.getTime() + daysToAdd * 86400000); 
+    const day = ("0" + firstDay.getDate()).slice(-2); 
+    const month = ("0" + (firstDay.getMonth() + 1)).slice(-2); 
+    const yearStr = firstDay.getFullYear(); 
+    return `${day}-${month}-${yearStr}`; 
+}
   
