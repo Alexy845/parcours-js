c595559e4fbb2a63df6ba3b9a5a755d939c83ba3
Alexy845 <alexy.daubresse@ynov.com>
2023-04-01 10:25:53 +0200

    update

---
+++ day-of-the-year.js
@@ -0,0 +1,7 @@
+function dayOfTheYear(date){
+    var start = new Date(date.getFullYear(), 0, 0);
+    var diff = date - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    var day = Math.floor(diff / oneDay);
+    return day;    
+}
