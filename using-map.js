18bc480eb20b7e234fc125cce89c7dacaa96489a
Alexy845 <alexy.daubresse@ynov.com>
2023-04-02 19:04:29 +0200

    update

--- using-map.js
+++ using-map.js
@@ -23,16 +23,14 @@
 const citiesOnly = arr => arr.map(item => item.city);
 const upperCasingStates = arr => arr.map(item => item.replace(/\b\w/g, letter => letter.toUpperCase()));
 
-const fahrenheitToCelsius = arr => {
-    return arr.map(item => {
-      const temp = parseFloat(item);
-      if (!isNaN(temp)) {
-        return Math.round((temp - 32) * 5 / 9) + '°C';
-      } else {
-        return 'Invalid temperature';
-      }
-    });
-  };
+function fahrenheitToCelsius(temps) {
+    const result = [];
+    for (let i = 0; i < temps.length; i++) {
+      const celsius = Math.floor((parseInt(temps[i]) - 32) * 5/9);
+      result.push(celsius.toString());
+    }
+    return result;
+}
   
 
   
