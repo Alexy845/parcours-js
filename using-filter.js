e6e626f089c58681dcd8412743250dda4b045580
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 00:04:36 +0200

    update

--- using-filter.js
+++ using-filter.js
@@ -5,3 +5,23 @@ const filterShortStateName = (states) => {
 const filterStartVowel = (states) => {
   return states.filter(state => /^[aeiou]/i.test(state));
 }
+
+const filter5Vowels = (states) => {
+  return states.filter(state => state.match(/[aeiou]/gi).length === 5);
+}
+
+const filter1DistinctVowel = (states) => {
+  return states.filter(state => {
+    const vowels = state.match(/[aeiou]/gi);
+    const distinctVowels = new Set(vowels);
+    return distinctVowels.size === 1;
+  });
+}
+
+const multiFilter = (states) => {
+  return states.filter(state => {
+    const vowels = state.match(/[aeiou]/gi);
+    const distinctVowels = new Set(vowels);
+    return distinctVowels.size === 1 && state.length > 7;
+  });
+}
