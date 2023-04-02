36fdd706bc0c4c3cc310cdbbf529d96725bcb55c
Alexy845 <alexy.daubresse@ynov.com>
2023-04-02 14:04:05 +0200

    update

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,7 +1,7 @@
 function dayOfTheYear(date) {
-  const startOfYear = new Date(date.getFullYear(), 0, 1);
-  const isLeapYear = ((date.getFullYear() % 4 === 0) && (date.getFullYear() % 100 !== 0)) || (date.getFullYear() % 400 === 0);
-  const diffInMs = date.getTime() - startOfYear.getTime();
-  const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-  return diffInDays + 1 + (isLeapYear && date.getMonth() > 1 ? 1 : 0);
-}
+    const startOfYear = new Date(date.getFullYear(), 0, 1); // le 1er janvier de l'année de la date
+    const diffInMilliseconds = date.getTime() - startOfYear.getTime(); // la différence entre la date et le 1er janvier en millisecondes
+    const diffInDays = diffInMilliseconds / (1000 * 60 * 60 * 24); // la différence en jours
+    return Math.floor(diffInDays) + 1; // on arrondit le nombre de jours et on ajoute 1 car le 1er janvier compte pour le premier jour de l'année
+  }
+  
