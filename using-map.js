8dd3d9095074660fd55022d1ab953d5c81d3a4ec
Alexy845 <alexy.daubresse@ynov.com>
2023-04-02 18:40:54 +0200

    update

--- using-map.js
+++ using-map.js
@@ -1,8 +1,24 @@
-function citiesOnly(arr) {
-  return arr.map(function (item) {
-    return item.city;
-  });
-}
+// function citiesOnly(arr) {
+//   return arr.map(function (item) {
+//     return item.city;
+//   });
+// }
+// function upperCasingStates(arr) {
+//     return arr.map(function (item) {
+//       return item.replace(/\b\w/g, function (match) {
+//         return match.toUpperCase();
+//       });
+//     });
+// }
 
+// function citiesOnly(array) {
+//     return array.map(cityobj => cityobj.city);
+// }
 
+// function upperCasingStates(array) {
+//     return array.map(state => {
+//         return state.replace(/\b\w/g, letter => letter.toUpperCase());
+//     });
+// }
+const citiesOnly = arr => arr.map(item => item.city);
 const upperCasingStates = arr => arr.map(item => item.replace(/\b\w/g, letter => letter.toUpperCase()));
