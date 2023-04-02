c759f7334383e0e4a24648ab8e183d5093eaf6e7
Alexy845 <alexy.daubresse@ynov.com>
2023-04-02 18:08:52 +0200

    update

--- using-map.js
+++ using-map.js
@@ -3,13 +3,9 @@ function citiesOnly(arr) {
     return item.city;
   });
 }
+
 function upperCasingStates(arr) {
-    return arr.map(function(item) {
-      if (item && item.state) {
-        return item.state.charAt(0).toUpperCase() + item.state.slice(1);
-      } else {
-        return "";
-      }
-    });
-  }
-  
+  return arr.map(function (item) {
+    return item.toUpperCase();
+  });
+}
