d46c956f2f7a09abc0e06884d49ffb36eb05ff9d
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 00:01:09 +0200

    update

--- using-filter.js
+++ using-filter.js
@@ -1,3 +1,7 @@
 const filterShortStateName = (states) => {
   return states.filter(state => state.length < 7);
 }
+
+const filterStartVowel = (states) => {
+  return states.filter(state => /^[aeiou]/i.test(state));
+}
