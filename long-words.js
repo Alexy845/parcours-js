db4a8e91d476992eedf7ba62dddc079ba9ba3daf
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 00:36:21 +0200

    update

--- long-words.js
+++ long-words.js
@@ -1,3 +1,4 @@
-const longWords = (tab) => {
-  return tab.filter((word) => word.length > 5);
-}
+const longWords = (arr) => {
+    return arr.every((elem) => typeof elem === 'string' && elem.length >= 5);
+  };
+  
