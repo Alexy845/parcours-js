94b5512600a60ebb682907e2521ec30e7776545e
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 00:43:22 +0200

    update

--- long-words.js
+++ long-words.js
@@ -2,6 +2,7 @@ const longWords = (arr) => {
     return arr.every((elem) => typeof elem === 'string' && elem.length >= 5);
   };
 
-const oneLongWord = (arr) => {
-    return arr.every((elem) => typeof elem ==='string' && elem.length >= 10);
-}
+  const oneLongWord = (arr) => {
+    return arr.some((elem) => typeof elem === 'string' && elem.length >= 10);
+  };
+  
