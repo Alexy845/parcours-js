df07d72b227df227033ca2425cfdd41c7ef03130
Alexy845 <alexy.daubresse@ynov.com>
2023-04-02 13:59:47 +0200

    update

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,7 +1,7 @@
 function dayOfTheYear(date) {
-    const startOfYear = new Date(date.getFullYear(), 0, 1);
-    const diffInMs = date.getTime() - startOfYear.getTime();
-    const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-    return diffInDays + 1;
-  }
-  
+  const startOfYear = new Date(date.getFullYear(), 0, 1);
+  const isLeapYear = ((date.getFullYear() % 4 === 0) && (date.getFullYear() % 100 !== 0)) || (date.getFullYear() % 400 === 0);
+  const diffInMs = date.getTime() - startOfYear.getTime();
+  const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+  return diffInDays + 1 + (isLeapYear && date.getMonth() > 1 ? 1 : 0);
+}
