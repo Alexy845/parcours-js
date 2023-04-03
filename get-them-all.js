b7325bd6461f51210745a7ec4084393b68737419
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 20:11:54 +0200

    update

--- get-them-all.js
+++ get-them-all.js
@@ -1,24 +1,37 @@
 export const getArchitects = () => {
-    const archi = Array.from(document.querySelectorAll('a'))
-    const nonArchi = Array.from(document.querySelectorAll(':not(a)'))
-    return [archi, nonArchi]
-  }
-  
-  export const getClassical = () => {
-    const classicalArchi = Array.from(document.querySelectorAll('.classical'))
-    const nonClassicalArchi = Array.from(document.querySelectorAll(':not(.classical)'))
-    return [classicalArchi, nonClassicalArchi]
-  }
-  
-  export const getActive = () => {
-    const activeClassicalArchi = Array.from(document.querySelectorAll('.classical.active'))
-    const nonActiveClassicalArchi = Array.from(document.querySelectorAll('.classical:not(.active)'))
-    return [activeClassicalArchi, nonActiveClassicalArchi]
-  }
-  
-  export const getBonannoPisano = () => {
-    const bonannoPisano = document.getElementById('BonannoPisano')
-    const activeClassicalArchiBonanno = Array.from(document.querySelectorAll('.classical.active:not(#BonannoPisano)'))
-    return [bonannoPisano, activeClassicalArchiBonanno]
-  }
-  
+    const arr = []
+    const archi = Array.from(document.getElementsByTagName('a'))
+    const nonArchi = Array.from(document.getElementsByTagName('span'))
+    arr.push(archi)
+    arr.push(nonArchi)
+    return arr
+} 
+
+export const getClassical = () => {
+    const arr =[]
+    const classiqueArchi = Array.from(document.getElementsByClassName('classical'))
+    const classiqueNonArchi = Array.from(document.querySelectorAll('a:not(.classical)'))
+    arr.push(classiqueArchi)
+    arr.push(classiqueNonArchi)
+    return arr
+}
+
+
+
+export const getActive = () => {
+    const arr = []
+    const classiqueActiveArchi = Array.from(document.getElementsByClassName('classical active'))
+    const classiqueNotActiveArchi = Array.from(document.querySelectorAll('a.classical:not(.active)'))
+    arr.push(classiqueActiveArchi)
+    arr.push(classiqueNotActiveArchi)
+    return arr
+}
+
+export const getBonannoPisano = () =>{
+    const arr = []
+    const BonnanoPisano = document.getElementById("BonannoPisano")
+    const nonBonnano = Array.from(document.querySelectorAll('a:not(#BonannoPisano)'))
+    arr.push(BonnanoPisano)
+    arr.push(nonBonnano)
+    return arr
+}
