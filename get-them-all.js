e3f3fe138b3e65ebc78a0ec8104e80da176dd746
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 19:57:55 +0200

    update

--- get-them-all.js
+++ get-them-all.js
@@ -1,24 +1,24 @@
 export const getArchitects = () => {
-    const architects = Array.from(document.querySelectorAll('a'))
-    const nonArchitects = Array.from(document.querySelectorAll(':not(a)'))
-    return [architects, nonArchitects]
+    const archi = Array.from(document.querySelectorAll('a'))
+    const nonArchi = Array.from(document.querySelectorAll(':not(a)'))
+    return [archi, nonArchi]
   }
   
   export const getClassical = () => {
-    const classicalArchitects = Array.from(document.querySelectorAll('.classical'))
-    const nonClassicalArchitects = Array.from(document.querySelectorAll(':not(.classical)'))
-    return [classicalArchitects, nonClassicalArchitects]
+    const classicalArchi = Array.from(document.querySelectorAll('.classical'))
+    const nonClassicalArchi = Array.from(document.querySelectorAll(':not(.classical)'))
+    return [classicalArchi, nonClassicalArchi]
   }
   
   export const getActive = () => {
-    const activeClassicalArchitects = Array.from(document.querySelectorAll('.classical.active'))
-    const nonActiveClassicalArchitects = Array.from(document.querySelectorAll('.classical:not(.active)'))
-    return [activeClassicalArchitects, nonActiveClassicalArchitects]
+    const activeClassicalArchi = Array.from(document.querySelectorAll('.classical.active'))
+    const nonActiveClassicalArchi = Array.from(document.querySelectorAll('.classical:not(.active)'))
+    return [activeClassicalArchi, nonActiveClassicalArchi]
   }
   
   export const getBonannoPisano = () => {
     const bonannoPisano = document.getElementById('BonannoPisano')
-    const activeClassicalArchitectsExceptBonanno = Array.from(document.querySelectorAll('.classical.active:not(#BonannoPisano)'))
-    return [bonannoPisano, activeClassicalArchitectsExceptBonanno]
+    const activeClassicalArchiBonanno = Array.from(document.querySelectorAll('.classical.active:not(#BonannoPisano)'))
+    return [bonannoPisano, activeClassicalArchiBonanno]
   }
   
