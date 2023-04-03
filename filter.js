3af26093c2de438f0b92819bc36954447b124137
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 09:53:40 +0200

    update

--- filter.js
+++ filter.js
@@ -17,3 +17,15 @@ const reject = (arr, fn) => {
     }
     return result;
   }
+
+const partition = (arr, fn) => {
+    const result = [[], []];
+    for (let i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i, arr)) {
+        result[0].push(arr[i]);
+      } else {
+        result[1].push(arr[i]);
+      }
+    }
+    return result;
+  }
