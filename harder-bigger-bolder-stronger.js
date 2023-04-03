241fc454a9648f603f2d1bb3e5089db9b615a3a0
Alexy845 <alexy.daubresse@ynov.com>
2023-04-03 22:24:12 +0200

    update

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -3,15 +3,15 @@ export function generateLetters(){
     for(let i=1;i<=120;i++){
         let letter = alphabet.charAt(Math.floor(Math.random() * alphabet.length))
         let div = document.createElement('div')
-        div.id = 'letter-' + letter
+        div.id = 'letter-'+letter
         div.innerHTML = letter
-        div.fontSize = (i+10)+'px';
+        div.style.fontSize = (i+10)+'px';
         if (i <= 40 ){
-            div.fontWeight = "300";
+            div.style.fontWeight = "300";
         } else if (i <= 80) {
-            div.fontWeight = "400";
+            div.style.fontWeight = "400";
         } else {
-            div.fontWeight = "600";
+            div.style.fontWeight = "600";
         }
         document.body.append(div)
     }
