0e2344e6a25f75ad3b85315271c30e2a4008b29b
Alexy845 <alexy.daubresse@ynov.com>
2023-04-05 22:43:27 +0200

    update

--- manipulate-values.js
+++ manipulate-values.js
@@ -1,25 +1,29 @@
 function filterValues(obj, fn) {
-  const result = {};
-  for (const key in obj) {
-    if (fn(obj[key])) {
-      result[key] = obj[key];
+    const result = {};
+    for (const key in obj) {
+        if (fn(obj[key])) {
+            result[key] = obj[key];
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 function mapValues(obj, fn) {
-  const result = {};
-  for (const key in obj) {
-    result[key] = fn(obj[key]);
-  }
-  return result;
+    const result = {};
+    for (const key in obj) {
+        result[key] = fn(obj[key]);
+    }
+    return result;
 }
 
 function reduceValues(obj, fn, acc) {
+    if (acc === undefined) {
+        acc = 0;
+    }
+
     let result = acc;
     for (const key in obj) {
         result = fn(result, obj[key]);
     }
     return result;
-    }
+}
