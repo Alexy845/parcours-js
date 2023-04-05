28866cabfdbf611fb3ba6be50ae86fabe881a9ab
Alexy845 <alexy.daubresse@ynov.com>
2023-04-05 21:30:43 +0200

    update

--- pick-omit.js
+++ pick-omit.js
@@ -13,17 +13,16 @@ function pick(obj, keys) {
 }
 function omit(obj, keys) {
     if (typeof keys === 'string') {
-        keys = [keys];
+      keys = [keys];
     }
-
+  
     const result = {};
     for (const key in obj) {
-        if (!keys.includes(key)) {
-            result[key] = obj[key];
-        }
+      if (obj.hasOwnProperty(key) && !keys.includes(key)) {
+        result[key] = obj[key];
+      }
     }
-    if (Object.keys(result).length === 0) {
-        return {};
-    }
+  
     return result;
-}
+  }
+  
