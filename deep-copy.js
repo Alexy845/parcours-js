90738208759cd0e427308a825bb6f71f9fe59e4c
Alexy845 <alexy.daubresse@ynov.com>
2023-04-06 21:07:18 +0200

    update

--- deep-copy.js
+++ deep-copy.js
@@ -1,12 +1,22 @@
 function deepCopy(obj) {
-  if (typeof obj !== 'object') {
-    return obj;
-  }
-  let newObj = obj instanceof Array ? [] : {};
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      newObj[key] = deepCopy(obj[key]);
+    if (typeof obj !== 'object' || obj === null) {
+      return obj;
     }
+    let newObj = obj instanceof Array ? [] : {};
+    if (obj instanceof RegExp) {
+      return new RegExp(obj);
+    }
+    if (obj instanceof Date) {
+      return new Date(obj.getTime());
+    }
+    if (obj instanceof Function) {
+      return obj;
+    }
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        newObj[key] = deepCopy(obj[key]);
+      }
+    }
+    return newObj;
   }
-  return newObj;
-}
+  
