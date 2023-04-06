4cc970a676c804e0dcc38428405803dbaa95c34b
Alexy845 <alexy.daubresse@ynov.com>
2023-04-06 21:32:58 +0200

    update

--- replica.js
+++ replica.js
@@ -1,10 +1,30 @@
-function replica(target, source) {
-  for (let key in source) {
-    if (typeof source[key] === 'object') {
-      target[key] = Array.isArray(source[key]) ? [] : {}
-      replica(target[key], source[key])
-    } else {
-      target[key] = source[key]
+function replica(target, ...sources) {
+    if (typeof target !== 'object' || target === null) {
+        throw new Error('Target must be an object');
     }
-  }
+
+    for (const source of sources) {
+        if (typeof source !== 'object' || source === null) {
+            continue;
+        }
+
+        for (const key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                const sourceValue = source[key];
+                const targetValue = target[key];
+
+                if (typeof sourceValue === 'object' && sourceValue !== null) {
+                    if (typeof targetValue !== 'object' || targetValue === null) {
+                        target[key] = Array.isArray(sourceValue) ? [] : {};
+                    }
+
+                    replica(target[key], sourceValue);
+                } else {
+                    target[key] = sourceValue;
+                }
+            }
+        }
+    }
+
+    return target;
 }
