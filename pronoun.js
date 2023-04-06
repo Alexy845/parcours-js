d3cb5c73d81d84e4f5e461c2d3651d32c4f5972f
Alexy845 <alexy.daubresse@ynov.com>
2023-04-06 21:49:57 +0200

    update

--- pronoun.js
+++ pronoun.js
@@ -12,10 +12,11 @@ function pronoun(str) {
             }
         } else if (currentPronoun) {
             result[currentPronoun].count++;
-            if (word[word.length - 1] === ',') {
+            if (word[word.length - 1] === ',' || word[word.length - 1] === '.') {
                 word = word.slice(0, word.length - 1);
             }
             result[currentPronoun].word.push(word);
+            currentPronoun = undefined;
         }
     });
 
