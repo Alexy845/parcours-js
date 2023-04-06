89ef27707afd2241a2151890c90be48bb05eb391
Alexy845 <alexy.daubresse@ynov.com>
2023-04-06 21:39:27 +0200

    update

--- pronoun.js
+++ pronoun.js
@@ -1,25 +1,26 @@
 function pronoun(str) {
     const pronouns = {
-      i: { word: [], count: 0 },
-      you: { word: [], count: 0 },
-      he: { word: [], count: 0 },
-      she: { word: [], count: 0 },
-      it: { word: [], count: 0 },
-      they: { word: [], count: 0 },
-      we: { word: [], count: 0 },
+        i: { word: [], count: 0 },
+        you: { word: [], count: 0 },
+        he: { word: [], count: 0 },
+        she: { word: [], count: 0 },
+        it: { word: [], count: 0 },
+        they: { word: [], count: 0 },
+        we: { word: [], count: 0 },
     };
     const words = str.split(' ');
     for (let i = 0; i < words.length - 1; i++) {
-      const word = words[i];
-      if (word.toLowerCase() in pronouns) {
-        const pronoun = pronouns[word.toLowerCase()];
-        const nextWord = words[i + 1];
-        if (nextWord) {
-          pronoun.word.push(nextWord);
+        const word = words[i];
+        if (word.toLowerCase() in pronouns) {
+            const pronoun = pronouns[word.toLowerCase()];
+            const nextWord = words[i + 1];
+            if (nextWord) {
+                pronoun.word.push(nextWord);
+            }
+            pronoun.count++;
         }
-        pronoun.count++;
-      }
     }
-    return pronouns;
-  }
-  
+    return Object.fromEntries(
+        Object.entries(pronouns).filter(([_, { count }]) => count > 0)
+    );
+}
