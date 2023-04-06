f52c94078eaa12bb8171d99117b2a0f41dac58f6
Alexy845 <alexy.daubresse@ynov.com>
2023-04-06 21:37:42 +0200

    update

---
+++ pronoun.js
@@ -0,0 +1,25 @@
+function pronoun(str) {
+    const pronouns = {
+      i: { word: [], count: 0 },
+      you: { word: [], count: 0 },
+      he: { word: [], count: 0 },
+      she: { word: [], count: 0 },
+      it: { word: [], count: 0 },
+      they: { word: [], count: 0 },
+      we: { word: [], count: 0 },
+    };
+    const words = str.split(' ');
+    for (let i = 0; i < words.length - 1; i++) {
+      const word = words[i];
+      if (word.toLowerCase() in pronouns) {
+        const pronoun = pronouns[word.toLowerCase()];
+        const nextWord = words[i + 1];
+        if (nextWord) {
+          pronoun.word.push(nextWord);
+        }
+        pronoun.count++;
+      }
+    }
+    return pronouns;
+  }
+  
