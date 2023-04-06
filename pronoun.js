ed173647a0cbc1185e91da42e0d4e0adeaa3cd83
Alexy845 <alexy.daubresse@ynov.com>
2023-04-06 21:43:31 +0200

    update

--- pronoun.js
+++ pronoun.js
@@ -1,24 +1,17 @@
-function pronoun(str) {
+function pronoun(text) {
     const pronouns = ['i', 'you', 'he', 'she', 'it', 'they', 'we'];
-    const words = str.split(/\s+/);
-    const result = {};
-    let currentPronoun;
-
-    words.forEach((word, index) => {
-        if (pronouns.includes(word.toLowerCase())) {
-            currentPronoun = word.toLowerCase();
-            if (!result[currentPronoun]) {
-                result[currentPronoun] = { word: [], count: 0 };
-            }
-        } else if (currentPronoun) {
-            result[currentPronoun].count++;
-            if (word[word.length - 1] === ',') {
-                word = word.slice(0, word.length - 1);
-            }
-            result[currentPronoun].word.push(word);
-            currentPronoun = undefined;
+    const words = text.toLowerCase().replace(/[\n,]/g, ' ').split(' ');
+    const counts = pronouns.reduce((acc, pronoun) => ({ ...acc, [pronoun]: { count: 0, word: [] } }), {});
+    
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (pronouns.includes(word)) {
+        counts[word].count++;
+        if (!counts[word].word.includes(words[i + 1])) {
+          counts[word].word.push(words[i + 1]);
         }
-    });
-
-    return result;
-}
+      }
+    }
+    return counts;
+  }
+  
